f713555f524ab39589ba74e48f2ffa683218aa68
harris <khanharris7@yahoo.com>
2021-05-31 22:39:17 -0500

    commit tree 63da0e34a62645258628fe0b6a42f038cbe6c522

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-// Code your solutions in this file
-const names = ['Lisa', 'Kaitlin', 'Jan'];
-const events = 'surprise';
-let thankYouCards = [];
+//const gifts = ["teddy bear", "drone", "doll"];
 
-function writeCards(names,events){
-    for (let x = 0; x < names.length; x++) {
-        thankYouCards.push(`Thank you ${names[x]}, for the wonderful ${events} gift!`);
+//function wrapGifts(gifts) {
+ // for (let i = 0; i < gifts.length; i++) {
+//    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//    debugger;
+ // }
+
+//  return gifts;
+//}
+
+//wrapGifts(gifts); 
+
+
+
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'surprise';
+let thankYouCards = [];
+function writeCards(names, event){
+    for (let x = 0; x < names.length; x++){
+        thankYouCards.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+        debugger;
     }
     return thankYouCards;
 }
+
+// Code your solutions in this file
+//const names = ['Lisa', 'Kaitlin', 'Jan'];
+//const events = 'surprise';
+//let thankYouCards = [];
+
+//function writeCards(names,events){
+ //   for (let x = 0; x < names.length; x++) {
+ //       thankYouCards.push(`Thank you, ${names[x]}, for the wonderful ${events} gift!`);
+     
+ //   }
+ //   return thankYouCards;
+//}
 writeCards(names,events);
-
-
-
-
 
 function countDown(number){
     let countDown = 10;
@@ -21,4 +45,6 @@ function countDown(number){
         console.log(countDown--);
     }
 }
-countDown(number)
+
+
+
